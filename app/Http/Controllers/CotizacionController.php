5cae3d96deae5e240ede6c392c95bdf5adffddf1
gilmormor <gilmormor@hotmail.com>
2020-04-08 17:01:17 -0400

    Cambios 08-04-2020

--- app/Http/Controllers/CotizacionController.php
+++ app/Http/Controllers/CotizacionController.php
@@ -610,7 +610,7 @@ class CotizacionController extends Controller
         
         $pdf = PDF::loadView('cotizacion.listado', compact('cotizacion','cotizacionDetalles','empresa'));
         //return $pdf->download('cotizacion.pdf');
-        return $pdf->stream($cotizacion->id .' - '. $cotizacion->cliente->razonsocial);
+        return $pdf->stream(str_pad($cotizacion->id, 4, "0", STR_PAD_LEFT) .' - '. $cotizacion->cliente->razonsocial . '.pdf');
         
     }
 }
