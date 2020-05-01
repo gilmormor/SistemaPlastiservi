14494c075dcaf2a07d8a62a058641f3f49cb1991
gilmormor <gilmormor@hotmail.com>
2020-05-01 17:57:53 -0400

    Mod PDF carta otro

--- app/Http/Controllers/NotaVentaController.php
+++ app/Http/Controllers/NotaVentaController.php
@@ -674,18 +674,16 @@ class NotaVentaController extends Controller
             //return view('notaventa.listado', compact('notaventa','notaventaDetalles','empresa'));
         
             $pdf = PDF::loadView('notaventa.listado', compact('notaventa','notaventaDetalles','empresa'));
-            $pdf->set_paper("A4", "portrait");
             //return $pdf->download('cotizacion.pdf');
-            return $pdf->stream(str_pad($notaventa->id, 5, "0", STR_PAD_LEFT) .' - '. $notaventa->cliente->razonsocial . '.pdf');
+            return $pdf->stream(str_pad($notaventa->id, 5, "0", STR_PAD_LEFT) .' - '. $notaventa->cliente->razonsocial . '.pdf')->set_paper("A4", "portrait");
     
         }else{
             if($stareport == '2'){
                 //return view('notaventa.listado1', compact('notaventa','notaventaDetalles','empresa'));
         
                 $pdf = PDF::loadView('notaventa.listado1', compact('notaventa','notaventaDetalles','empresa'));
-                $pdf->set_paper("A4", "portrait");
                 //return $pdf->download('cotizacion.pdf');
-                return $pdf->stream(str_pad($notaventa->id, 5, "0", STR_PAD_LEFT) .' - '. $notaventa->cliente->razonsocial . '.pdf');
+                return $pdf->stream(str_pad($notaventa->id, 5, "0", STR_PAD_LEFT) .' - '. $notaventa->cliente->razonsocial . '.pdf')->set_paper("A4", "portrait");
     
             }
         }
