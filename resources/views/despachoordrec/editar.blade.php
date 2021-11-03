ef52fe278bd9b270904d95df65ef8d9d94350f75
gilmormor <gilmormor@hotmail.com>
2021-11-03 17:39:59 -0300

    Corregir titulo Orden de compra en pantallas

--- resources/views/despachoordrec/editar.blade.php
+++ resources/views/despachoordrec/editar.blade.php
@@ -30,7 +30,7 @@
                 <a class='btn-accion-tabla btn-sm' onclick='genpdfNV({{$data->notaventa_id}},1)' title='Ver Nota venta' data-toggle='tooltip'>
                     Nota Venta: {{$data->notaventa_id}} <i class='fa fa-fw fa-file-pdf-o'></i>
                 </a>
-                <a class='btn-accion-tabla btn-sm' onclick='verpdf2("{{$data->notaventa->oc_file}}",2)' title='Ver Nota venta' data-toggle='tooltip'>
+                <a class='btn-accion-tabla btn-sm' onclick='verpdf2("{{$data->notaventa->oc_file}}",2)' title='Orden de Compra' data-toggle='tooltip'>
                     Orden Compra: {{$data->notaventa->oc_id}} <i class='fa fa-fw fa-file-pdf-o'></i>
                 </a>
                 <div class="box-tools pull-right">
