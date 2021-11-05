1260e0fa84aa9a7aa931e80d6e95289a1c0c1c88
gilmormor <gilmormor@hotmail.com>
2021-11-05 19:13:54 -0300

    Corregir nombre de DespachoOrdRecaprController

--- app/Http/Controllers/DespachoOrdrecAprController.php
+++ app/Http/Controllers/DespachoOrdrecAprController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class DespachoOrdrecAprController extends Controller
+class DespachoOrdRecAprController extends Controller
 {
     /**
      * Display a listing of the resource.
