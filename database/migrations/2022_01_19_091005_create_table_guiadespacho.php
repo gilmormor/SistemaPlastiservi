12c41715399dcc28cf72795510f10729f17fa7a2
gilmormor <gilmormor@hotmail.com>
2022-03-16 12:29:00 -0300

    cambios buia despacho

--- database/migrations/2022_01_19_091005_create_table_guiadespacho.php
+++ database/migrations/2022_01_19_091005_create_table_guiadespacho.php
@@ -20,19 +20,39 @@ class CreateTableGuiadespacho extends Migration
             $table->foreign('despachoord_id','fk_guiadespacho_despachoord')->references('id')->on('despachoord')->onDelete('restrict')->onUpdate('restrict');
             $table->unsignedBigInteger('notaventa_id');
             $table->foreign('notaventa_id','fk_guiadespacho_notaventa')->references('id')->on('notaventa')->onDelete('restrict')->onUpdate('restrict');
-            $table->unsignedBigInteger('usuario_id')->comment('Usuario que creo el registro');
-            $table->foreign('usuario_id','fk_guiadespacho_usuario')->references('id')->on('usuario')->onDelete('restrict')->onUpdate('restrict');
             $table->dateTime('fechahora')->comment('Fecha y hora.');
             $table->unsignedBigInteger('comunaentrega_id')->comment('Comuna de entrega');
             $table->foreign('comunaentrega_id','fk_guiadespacho_comunaentrega')->references('id')->on('comuna')->onDelete('restrict')->onUpdate('restrict');
             $table->unsignedBigInteger('tipoentrega_id');
             $table->foreign('tipoentrega_id','fk_guiadespacho_tipoentrega')->references('id')->on('tipoentrega')->onDelete('restrict')->onUpdate('restrict');
-            $table->date('plazoentrega')->comment('Plazo de entrega fecha');
+            $table->unsignedBigInteger('sucursal_id')->comment("Id de Sucursal");
+            $table->foreign('sucursal_id','fk_guiadespacho_sucursal')->references('id')->on('sucursal')->onDelete('restrict')->onUpdate('restrict');
+            $table->unsignedBigInteger('cliente_id');
+            $table->foreign('cliente_id','fk_guiadespacho_cliente')->references('id')->on('cliente')->onDelete('restrict')->onUpdate('restrict');
+            $table->string('rut',12)->comment('RUT Cliente');
+            $table->string('razonsocial',70)->comment('Nombre cliente');
+            $table->string('giro',100)->comment('Giro');
+            $table->string('dircliente',200)->comment('Direccion cliente.');
+            $table->string('comuna',100)->comment('Comuna.');
+            $table->string('ciudad',100)->comment('Ciudad.');
+            $table->unsignedBigInteger('comuna_id');
+            $table->foreign('comuna_id','fk_guiadespacho_comuna')->references('id')->on('comuna')->onDelete('restrict')->onUpdate('restrict');
+
+            /*$table->date('plazoentrega')->comment('Plazo de entrega fecha');*/
             $table->string('lugarentrega',100)->comment('Lugar de entrega');
             $table->string('contacto',50)->comment('Nombre de contacto');
             $table->string('contactoemail',50)->comment('Email de contacto de entrega')->nullable();
             $table->string('contactotelf',50)->comment('Telefono de contacto de entregao')->nullable();
-            $table->string('observacion',200)->comment('Observaciones')->nullable();
+            $table->string('oc_id',15)->comment('Numero de Orden de Compra')->nullable();
+            $table->string('oc_file',20)->comment('Archivo o imagen de Orden de Compra')->nullable();
+            $table->string('obs',200)->comment('Observaciones')->nullable();
+            $table->dateTime('anulada')->comment('Fecha de anulación')->nullable();
+            $table->float('piva',5,2)->comment('Porcentaje IVA')->nullable()->after('neto');
+            $table->float('neto',18,2)->comment('Total neto, Valor sin IVA');
+            $table->float('iva',18,2)->comment('Total IVA');
+            $table->float('total',18,2)->comment('Total incluye IVA');
+
+            /*
             $table->date('fechaestdesp')->comment('Fecha estimada de Despacho.');
             $table->string('guiadespacho',50)->comment('Guia despacho')->nullable();
             $table->dateTime('guiadespachofec')->comment('Fecha inclusion guia despacho.')->nullable();
@@ -41,8 +61,11 @@ class CreateTableGuiadespacho extends Migration
             $table->dateTime('numfacturafec')->comment('Fecha inclusion numero de factura.')->nullable();
             $table->unsignedBigInteger('despachoobs_id')->nullable();
             $table->foreign('despachoobs_id','fk_guiadespacho_despachoobs')->references('id')->on('despachoobs')->onDelete('restrict')->onUpdate('restrict');
-            $table->boolean('aprguiadesp')->comment('Aprobar Guia Despacho (null o 0)=Sin aprobar, 1=Registro aprobado ingresar Guia Despacho.')->nullable();
-            $table->dateTime('aprguiadespfh')->comment('Aprobar Guia Despacho Fecha hora cuando fue aprobado para ingresar Guia de espacho.')->nullable();
+            $table->boolean('aprobstatus')->comment('Status de aprobacion (null o 0)=Sin aprobar, 1=enviar a aprobacion, 2=aprobada, 3=Rechazada')->nullable();
+            $table->dateTime('aprobfechahora')->comment('Fecha y hora cuando fue aprobada.')->nullable();
+            */
+            $table->unsignedBigInteger('usuario_id')->comment('Usuario que creo el registro');
+            $table->foreign('usuario_id','fk_guiadespacho_usuario')->references('id')->on('usuario')->onDelete('restrict')->onUpdate('restrict');
             $table->unsignedBigInteger('usuariodel_id')->comment('ID Usuario que elimino el registro')->nullable();
             $table->timestamps();
             $table->softDeletes();
--- database/migrations/2022_01_19_091032_create_table_guiadespachodet.php
+++ database/migrations/2022_01_19_091032_create_table_guiadespachodet.php
@@ -22,7 +22,16 @@ class CreateTableGuiadespachodet extends Migration
             $table->foreign('despachoorddet_id','fk_guiadespachodet_despachoorddet')->references('id')->on('despachoorddet')->onDelete('restrict')->onUpdate('restrict');
             $table->unsignedBigInteger('notaventadetalle_id');
             $table->foreign('notaventadetalle_id','fk_guiadespachodet_notaventadetalle')->references('id')->on('notaventadetalle')->onDelete('restrict')->onUpdate('restrict');
-            $table->float('cantdesp',10,2)->comment('Cantidad despacho.')->nullable();
+            $table->unsignedBigInteger('producto_id');
+            $table->foreign('producto_id','fk_guiadespachodet_producto')->references('id')->on('producto')->onDelete('restrict')->onUpdate('restrict');
+            $table->string('desc1',100)->comment('Descripcion 1');
+            $table->string('desc2',100)->comment('Descripcion 2');
+            $table->float('cant',10,2)->comment('Cantidad despacho.')->nullable();
+            $table->string('unimed',10)->comment('Unidad de medida.');
+            $table->float('preciounit',18,2)->comment('Precio Unitario sin IVA');
+            $table->string('adicional',10)->comment('Adicional.');
+            $table->float('descuento',5,2)->comment('Porcentaje Descuento por renglon.');
+            $table->float('subtotal',18,2)->comment('SubTotal Precio neto (cant x preciounit) sin IVA');
             $table->unsignedBigInteger('usuariodel_id')->comment('ID Usuario que elimino el registro')->nullable();
             $table->timestamps();
             $table->softDeletes();
