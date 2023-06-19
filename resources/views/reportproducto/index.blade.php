e1c4af9059312b8c545862c1232faa2e17a4a31d
gilmormor <gilmormor@hotmail.com>
2023-06-19 18:02:57 -0400

    Corregir class="fa fa-minus" Rep Prod y Clientes

--- resources/views/reportproducto/index.blade.php
+++ resources/views/reportproducto/index.blade.php
@@ -23,7 +23,7 @@ Reporte Productos
             <div class="box-header with-border">
                 <h3 class="box-title">Reporte Productos</h3>
                 <div class="box-tools pull-right">
-                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i></button>
+                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
                 </div>
             </div>
             <div class="box-body">
