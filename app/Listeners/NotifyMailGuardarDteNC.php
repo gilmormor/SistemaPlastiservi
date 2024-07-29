033c7576b7a3067bdaff8d646652c155988b9f19
gilmormor <gilmormor@hotmail.com>
2024-07-29 12:37:08 -0400

    Agregar a una persona mas para Enviar correo NC y ND

--- app/Listeners/NotifyMailGuardarDteNC.php
+++ app/Listeners/NotifyMailGuardarDteNC.php
@@ -10,7 +10,6 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Mail;
 
-
 class NotifyMailGuardarDteNC
 {
     /**
@@ -31,32 +30,37 @@ class NotifyMailGuardarDteNC
      */
     public function handle(GuardarDteNC $event)
     {
-        $rutaPantalla = urlPrevio();
-        $rutaOrigen = urlActual();
         $dte = $event->dte;
-        $notificaciones = new Notificaciones();
-        $notificaciones->usuarioorigen_id = auth()->id();
         //POR AHORA VOY A ENVIAR EL CORREO SOLO A ANGEL ID:66, LUEGO DEBO RECORREOS TODOS LOS USUARIOS QUE DEBO ENVIAR EL CORREO
-        $persona = Persona::findOrFail(66);
-        $aux_email = $persona->email;
-        $notificaciones->usuariodestino_id = $persona->usuario->id;
-        $notificaciones->vendedor_id = $dte->vendedor_id;
-        $notificaciones->status = 1;                    
-        $notificaciones->nombretabla = 'dte';
-        $notificaciones->mensaje = 'Nota Credito: '.$dte->nrodocto;
-        $notificaciones->nombrepantalla = $rutaPantalla; //'despachoord.indexguiafact';
-        $notificaciones->rutaorigen = $rutaOrigen; //'despachoord/indexfactura';
-        $notificaciones->rutadestino = 'reportdtenc';
-        $notificaciones->tabla_id = $dte->id;
-        $notificaciones->accion = 'NC ' . $dte->nrodocto . ' emitida.';
-        $notificaciones->mensajetitle = 'Nro. NC: '.$dte->nrodocto;
-        $notificaciones->icono = 'fa fa-fw fa-minus-square-o text-red';
-        $notificaciones->save();
-        //$usuario = Usuario::findOrFail(auth()->id());
-        $asunto = $notificaciones->mensaje;
-        $cuerpo = $notificaciones->mensaje;
-
-        Mail::to($aux_email)->send(new MailDteNC($notificaciones,$asunto,$cuerpo,$dte));
-
+        enviarcorreo($dte,66);
+        enviarcorreo($dte,71);
     }
+}
+
+function enviarcorreo($dte,$persona_id){
+    $persona = Persona::findOrFail($persona_id);
+    $rutaPantalla = urlPrevio();
+    $rutaOrigen = urlActual();
+    $notificaciones = new Notificaciones();
+    $notificaciones->usuarioorigen_id = auth()->id();
+    $aux_email = $persona->email;
+    $notificaciones->usuariodestino_id = $persona->usuario->id;
+    $notificaciones->vendedor_id = $dte->vendedor_id;
+    $notificaciones->status = 1;                    
+    $notificaciones->nombretabla = 'dte';
+    $notificaciones->mensaje = 'Nota Credito: '.$dte->nrodocto;
+    $notificaciones->nombrepantalla = $rutaPantalla; //'despachoord.indexguiafact';
+    $notificaciones->rutaorigen = $rutaOrigen; //'despachoord/indexfactura';
+    $notificaciones->rutadestino = 'reportdtenc';
+    $notificaciones->tabla_id = $dte->id;
+    $notificaciones->accion = 'NC ' . $dte->nrodocto . ' emitida.';
+    $notificaciones->mensajetitle = 'Nro. NC: '.$dte->nrodocto;
+    $notificaciones->icono = 'fa fa-fw fa-minus-square-o text-red';
+    $notificaciones->save();
+    //$usuario = Usuario::findOrFail(auth()->id());
+    $asunto = $notificaciones->mensaje;
+    $cuerpo = $notificaciones->mensaje;
+
+    Mail::to($aux_email)->send(new MailDteNC($notificaciones,$asunto,$cuerpo,$dte));
+
 }
