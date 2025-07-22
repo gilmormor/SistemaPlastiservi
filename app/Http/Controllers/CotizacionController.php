af233ba8e3ab4ddffd2754110dd3a0e9443cdeba
gilmormor <gilmormor@hotmail.com>
2025-07-22 17:39:22 -0400

    Activar envio de correo cuando hay que validar Acuerdo Tecnico

--- app/Http/Controllers/CotizacionController.php
+++ app/Http/Controllers/CotizacionController.php
@@ -935,7 +935,7 @@ class CotizacionController extends Controller
             */
             if ($cotizacion->save()) {
                 if($aux_statusAcuTec){
-                    //Event(new AvisoRevisionAcuTec($cotizacion));
+                    Event(new AvisoRevisionAcuTec($cotizacion));
                 }    
                 return response()->json([
                     'error' => 0,
