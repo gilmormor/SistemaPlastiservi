f1fe8bcc18a0cc2b491153806816740713f776c6
gilmormor <gilmormor@hotmail.com>
2026-02-12 13:14:25 -0300

    Actualizar script de búsqueda de producto en la vista de creación de factura

--- resources/views/dtefacturadir/crear.blade.php
+++ resources/views/dtefacturadir/crear.blade.php
@@ -17,7 +17,7 @@
     <script src="{{autoVer("assets/pages/scripts/general.js")}}" type="text/javascript"></script>
     <script src="{{autoVer("assets/pages/scripts/dtefacturadir/crear.js")}}" type="text/javascript"></script>
     <script src="{{autoVer("assets/pages/scripts/cliente/buscarnew.js")}}" type="text/javascript"></script>
-    <script src="{{autoVer("assets/pages/scripts/producto/buscarnew.js")}}" type="text/javascript"></script>
+    <script src="{{autoVer("assets/pages/scripts/producto/buscar.js")}}" type="text/javascript"></script>
 @endsection
 
 @section('contenido')
